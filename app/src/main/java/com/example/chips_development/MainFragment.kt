5e9662d109c40e6b3f2229870f1315aeac398819
KroneXI <mihailleonov161@gmail.com>
2022-11-08 23:12:14 +0300

    в activity_main.xml добавлен элемент фрагмента контейнера добавлен HelpFragment.kt, fragment_help.xml (фрагмент с будующей реализацией информации о разработчиках) добавлен MainFragment.kt, fragment_main.xml (главный фрагмент) добавлен main_nav.xml (содержит навигацию) в MainActivity.kt инициализирован контроллер навигации navController

---
+++ app/src/main/java/com/example/chips_development/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.chips_development
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =  inflater.inflate(R.layout.fragment_main, container, false)
+        val button = view.findViewById<Button>(R.id.buttonToHelp)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_helpFragment)
+        }
+
+        return view
+    }
+
+//    companion object {}
+}
