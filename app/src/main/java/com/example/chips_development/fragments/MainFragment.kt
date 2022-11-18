4372cfaa34e9c4c9079799d1fe7b63b3bc6a2e71
KroneXI <mihailleonov161@gmail.com>
2022-11-19 00:02:09 +0300

    что-то в fragment_test.xml что-то в fragment_main.xml что-то в MainFragment.kt

--- app/src/main/java/com/example/chips_development/fragments/MainFragment.kt
+++ app/src/main/java/com/example/chips_development/fragments/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -13,6 +14,10 @@ import com.example.chips_development.R
 class MainFragment : Fragment() {
 //    private var button : Button? = null
 //    private var textView : TextView? = null
+//    private val verticalProgressbar: ProgressBar = itemView.findViewById(R.id.verticalProgressbar)
+    private var verticalProgressbar: ProgressBar? = null
+    private var verticalProgressbar1: ProgressBar? = null
+    private var verticalProgressbar2: ProgressBar? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,13 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        verticalProgressbar = view.findViewById(R.id.verticalProgressbar)
+        verticalProgressbar1 = view.findViewById(R.id.verticalProgressbar1)
+        verticalProgressbar2 = view.findViewById(R.id.progress_bar)
+
+        verticalProgressbar?.progress = 90
+        verticalProgressbar1?.progress = 40
+        verticalProgressbar2?.progress = 30
 //        button = view.findViewById(R.id.buttonToHelp)
 //        button?.setOnClickListener(buttonsClickListener)
 
